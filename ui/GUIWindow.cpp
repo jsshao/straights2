7c69f4d7dafcd21f7b1881e63ba93d924e75910b
hpp3 <ewgao@uwaterloo.ca>
2014-07-19 00:44:47 -0400

    fixed segfault

--- ui/GUIWindow.cpp
+++ ui/GUIWindow.cpp
@@ -8,7 +8,7 @@ GUIWindow::GUIWindow() :
     table.set_row_spacings(10);
     table.set_col_spacings(10);
     set_border_width( 10 );
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 13; j++) {
             cards[i*13+j] = new Gtk::Image(card_manager.getCardImage((Faces)j, (Suits)i));
             table.attach(*cards[i*13+j], j, j+1, i, i+1);
